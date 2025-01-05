f8a54f74229c922fb35967fd15aebe80137d73a5
J_Mandoo <JJinMandoo1014@gmail.com>
2025-01-05 12:59:02 +0900

    login demo

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main_page.dart';
+import 'signup_page.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -8,17 +9,32 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('login')),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              prefs.setBool('isLoggedIn', true);
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainPage()),
-              );
-            },
-            child: Text('login')
-        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  prefs.setBool('isLoggedIn', true);
+                  Navigator.pushReplacement(
+                      context, 
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                }, 
+                child: Text('로그인')
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => SignupPage()),
+                  );
+                }, 
+                child: Text('회원가입')
+            )
+          ],
+        )
       ),
     );
   }
